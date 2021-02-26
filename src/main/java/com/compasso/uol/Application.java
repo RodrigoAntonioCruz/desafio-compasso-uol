7d0df8d280236867f3a2bc885cd3dfb87fff98dc
RodrigoAntonioCruz <rodrigoantoniocruz134@gmail.com>
2021-02-25 21:27:11 -0300

    Desafio Compasso UOL

--- src/main/java/com/compasso/uol/Application.java
+++ src/main/java/com/compasso/uol/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioCompassoUol1Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioCompassoUol1Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
