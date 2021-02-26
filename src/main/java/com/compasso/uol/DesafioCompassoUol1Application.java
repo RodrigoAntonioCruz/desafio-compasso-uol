795bdd4fd582d9d9c6d93b02e62deb692f96eb05
RodrigoAntonioCruz <rodrigoantoniocruz134@gmail.com>
2021-02-25 21:13:29 -0300

    Desafio Compasso UOL
    Inicio do desenvolvimento

---
+++ src/main/java/com/compasso/uol/DesafioCompassoUol1Application.java
@@ -0,0 +1,13 @@
+package com.compasso.uol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCompassoUol1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCompassoUol1Application.class, args);
+	}
+
+}
