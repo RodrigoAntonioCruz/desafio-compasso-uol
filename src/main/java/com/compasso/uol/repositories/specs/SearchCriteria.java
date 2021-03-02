b11da5a5c992105cf9446de4c69e3e73764fb498
RodrigoAntonioCruz <rodrigoantoniocruz134@gmail.com>
2021-03-02 20:46:12 -0300

    Desafio Compasso UOL
    refinamento dos filtros utilizando vários critérios de busca

--- src/main/java/com/compasso/uol/repositories/specs/SearchCriteria.java
+++ src/main/java/com/compasso/uol/repositories/specs/SearchCriteria.java
@@ -1,51 +1,61 @@
 package com.compasso.uol.repositories.specs;
 
 public class SearchCriteria {
-    private String key;
-    private Object value;
-    private SearchOperation operation;
+	private String k;
+	private String key;
+	private Object value;
+	private SearchOperation operation;
 
-    public SearchCriteria() {
-    }
+	public SearchCriteria() {
+	}
 
-    public SearchCriteria(String key, Object value, SearchOperation operation) {
-        this.key = key;
-        this.value = value;
-        this.operation = operation;
-    }
+	public SearchCriteria(String key, Object value, SearchOperation operation) {
+		this.key = key;
+		this.value = value;
+		this.operation = operation;
+	}
 
-    // getters and setters, equals(), toString(), ... (omitted for brevity)
+	public SearchCriteria(String key, String k, Object value, SearchOperation operation) {
+		this.key = key;
+		this.k = k;
+		this.value = value;
+		this.operation = operation;
+	}
 
-    public String getKey() {
-        return key;
-    }
+	public String getKey() {
+		return key;
+	}
 
-    public void setKey(String key) {
-        this.key = key;
-    }
+	public void setKey(String key) {
+		this.key = key;
+	}
 
-    public Object getValue() {
-        return value;
-    }
+	public String getK() {
+		return k;
+	}
 
-    public void setValue(Object value) {
-        this.value = value;
-    }
+	public void setK(String k) {
+		this.k = k;
+	}
 
-    public SearchOperation getOperation() {
-        return operation;
-    }
+	public Object getValue() {
+		return value;
+	}
 
-    public void setOperation(SearchOperation operation) {
-        this.operation = operation;
-    }
+	public void setValue(Object value) {
+		this.value = value;
+	}
 
-    @Override
-    public String toString() {
-        return "SearchCriteria{" +
-                "key='" + key + '\'' +
-                ", value=" + value +
-                ", operation=" + operation +
-                '}';
-    }
+	public SearchOperation getOperation() {
+		return operation;
+	}
+
+	public void setOperation(SearchOperation operation) {
+		this.operation = operation;
+	}
+
+	@Override
+	public String toString() {
+		return "SearchCriteria{" + "k='" + key + '\'' + "key='" + key + '\'' + ", value=" + value + ", operation=" + operation + '}';
+	}
 }
